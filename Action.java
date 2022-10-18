0cb4ed47fd6062e0bec89f586e0e7a756d3fe8ab
Eric Avila <arnoldavila2001@gmail.com>
2022-10-18 09:43:29 -0400

    Stub1

---
+++ Action.java
@@ -0,0 +1,12 @@
+public enum Action {
+     KAYAKING,
+     ZIPLINING,
+     ROCK_WALL,
+     ARTS_AND_CRAFTS,
+     FIELD_GAMES,
+     HIKING,
+     SCAV_HUNT,
+     BREAKFAST,
+     LUNCH,
+     DINNER
+}
---
+++ FAQ.java
@@ -0,0 +1,17 @@
+public class FAQ {
+    public String question;
+    public String answer;
+
+    public FAQ (String question, String answer) {
+
+    }
+
+    public String getQuestion() {
+        return " ";
+    }
+
+    public String getAnswer() {
+        return " ";
+    }
+
+}
