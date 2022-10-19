4afcb077a39d775edfed8ac6510b267c58d13a1d
Marcus Campbell <lilmohawk5052@gmail.com>
2022-10-19 11:57:40 -0400

    added some constants and updated director phoneNumber to phoneNum

--- DataConstants.java
+++ DataConstants.java
@@ -1,3 +1,9 @@
 public class DataConstants {
-    
+    protected static final String cabinFile = "json/childs.json";
+    protected static final String parentFile = "json/parents.json";
+    protected static final String directorFile = "json/director.json";
+    protected static final String childsFile = "json/childs.json";
+    protected static final String firstName = "firstName";
+    protected static final String lastName = "lastName";
+    protected static final String phoneNumber = "phoneNum";
 }
