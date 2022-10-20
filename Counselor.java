152b09de6d39522e1c48c9b9d75d23e4c3c6bd4c
ava <avaturner03@gmail.com>
2022-10-20 08:41:30 -0400

    allergy array list

--- Counselor.java
+++ Counselor.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 public class Counselor extends User{
     private String aboutMe;
     private String gender;
-    private String allergy;
     private String shirtSize;
     private int strike;
     public String medName;
     public String medtime;
     public String medDose;
     private ArrayList<Contact> contacts;
+    public ArrayList<String> allergy;
+
 
     public Counselor(String firstName, String lastName, String email, String password, String phoneNum, String birthday, String street
-    , String town, String state, int zipCode, String country, String gender, String allergy, String shirtSize) {
+    , String town, String state, int zipCode, String country, String gender, String shirtSize) {
         super(firstName, lastName, email, password, phoneNum, birthday, street, town, state, zipCode, country);
         this.gender = gender;
-        this.allergy = allergy;
         this.shirtSize = shirtSize;
+        contacts = new ArrayList();
+        allergy = new ArrayList();    
     }
 
     public void addMedication(String medName, String medTime, String medDose){
