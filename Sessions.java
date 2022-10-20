fb94d6bafb28e4baa01807011facbdda06b4c120
ava <avaturner03@gmail.com>
2022-10-20 09:02:18 -0400

    sessions class

---
+++ Sessions.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Sessions {
+    public String theme;
+    public ArrayList<Cabin> cabins;
+
+    public Sessions(String theme){
+        this.theme = theme;
+    }
+
+    public void addCabin (Cabin cabin){
+
+    }
+}
