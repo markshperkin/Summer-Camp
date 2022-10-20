da9ada25785e744a7b5e3f7f3f41c69b7894e8cc
Marcus Campbell <marcusacampbell03@gmail.com>
2022-10-20 09:33:44 -0400

    DataReader update

--- DataReader.java
+++ DataReader.java
@@ -1,12 +1,31 @@
+import java.io.FileReader;
 import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class DataReader extends DataConstants{
     
-    public ArrayList<User> getAllUsers(){
+    public static ArrayList<Parent> getAllParents(){
+        ArrayList<Parent> parents = new ArrayList<Parent>();
+
+        try {
+            FileReader parentReader = new FileReader(parentFile);
+            JSONParser parser = new JSONParser();
+            JSONArray parentJSON = (JSONArray)new JSONParser().parse(parentReader);
+        }
         return null;
     }
     
-    public ArrayList<Activity> getAllActivities() {
+    public static ArrayList<Child> getAllChilds() {
+        return null;
+    }
+
+    public static ArrayList<Director> getAllDirectors() {
+        return null;
+    }
+
+    public static ArrayList <Counselor> getAllConselors() {
         return null;
     }
 }
