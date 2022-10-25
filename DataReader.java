d3c1709aa4e15a50c92dd1c0f7be0d2b0f1a54c2
Marcus Campbell <marcusacampbell03@gmail.com>
2022-10-25 08:37:28 -0400

    added ) to end of line 134 DataReader.java

--- DataReader.java
+++ DataReader.java
@@ -131,7 +131,7 @@ public class DataReader extends DataConstants{
                 ArrayList <Contact> contacts = new ArrayList<Contact>(); 
             
 
-                contacts.addAll(counselorsJSON.get("contacts");
+                contacts.addAll(counselorsJSON.get("contacts"));
 
                
         counselors.add(new Counselor(fname, lname, email, password, phoneNum, birthday, street, town, state, i, country, gender, SSize));
