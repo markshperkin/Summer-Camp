b4d7c0196799dc4023933aecbfedd0f45241f004
ava <avaturner03@gmail.com>
2022-10-27 09:36:54 -0400

    creating a session

--- Sessions.java
+++ Sessions.java
@@ -1,15 +1,33 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Sessions {
-    public String theme;
+    public String [] themes;
+    public String weeklyTheme;
     public ArrayList<Cabin> cabins;
+    Random ranNum = new Random();
+    Cabin cabin = new Cabin();
 
-    public Sessions(String theme){
-        this.theme = theme;
+    public Sessions(String weeklyTheme, ArrayList<Cabin> cabins){
+        this.weeklyTheme = weeklyTheme;
+        cabins = new ArrayList();
+        weeklyTheme = new String();
     }
 
-    public String getTheme() {
-        return theme;
+    public Sessions(){}
+    
+    public void addThemes() {
+        themes[0] = "splash week";
+        themes[1] = "lost in space";
+        themes[2] = "under the sea";
+        themes[3] = "enchanted forest";
+        themes[4] = "pirates";
+        themes[5] = "safari season";
+        themes[6] = "superheros";
+        themes[7] = "dinosaur week";
+        themes[8] = "treasure hunt";
+        themes[9] = "shark week";
+
     }
 
     public void addCabin (Cabin cabin){
@@ -17,6 +35,10 @@ public class Sessions {
         
     }
 
-    
+    public void createSession(){
+        for (int x = 0; x < 10; x++){
+            Sessions session = new Sessions(themes[x], cabin.getCabins);
+        }
+    }
 
 }
