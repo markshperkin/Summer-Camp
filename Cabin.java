14788c22dd747ddde833f29c2e1604be411ea15f
Eric Avila <arnoldavila2001@gmail.com>
2022-10-27 01:37:39 -0400

    updated cabin

--- Cabin.java
+++ Cabin.java
@@ -7,34 +7,44 @@ public class Cabin {
     private ArrayList<Child> campers;
     public Random rand;
     private Counselor counselor;
+    private int ageRange;
     
     
 
-    public Cabin (Counselor counselor, ArrayList<Child> campers){
-        
-      
+    public Cabin (Counselor counselor, ArrayList<Child> campers, int ageRange){
+        this.ageRange = ageRange;
         this.counselor = counselor;
         rand = new Random();
+        schedule = new HashMap<Days,ArrayList<Activity>>();
+    }
+
+    public Counselor getCounselor() {
+        return counselor;
+
     }
 
     public void CreateSchedule(Activity activity){
 
 
+
         schedule.put(Days.MONDAY, new ArrayList<Activity>());
-        schedule.put(Days.TUESDAY, null);
-        schedule.put(Days.WEDNESDAY, null);
-        schedule.put(Days.THURSDAY, null);
-        schedule.put(Days.FRIDAY, null);
+        schedule.put(Days.TUESDAY, new ArrayList<Activity>());
+        schedule.put(Days.WEDNESDAY, new ArrayList<Activity>());
+        schedule.put(Days.THURSDAY, new ArrayList<Activity>());
+        schedule.put(Days.FRIDAY, new ArrayList<Activity>());
     }
 
     public void addCamper(Child camper){
-
+//up to nine campers
         campers.add(camper);
     }
 
     public static String viewSchedule(){
-        return "schedule";
+        return "Schedule";
     }
     
+    public boolean withinAgeRange() {
+        return true;
+    }
 
 }
