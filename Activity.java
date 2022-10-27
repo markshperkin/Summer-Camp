b01c3db0ef49345488f1fc71ceb48b1844825d62
Eric Avila <arnoldavila2001@gmail.com>
2022-10-27 02:33:36 -0400

    trying my best to do cabin stuff

--- Activity.java
+++ Activity.java
@@ -1,4 +1,10 @@
-import javax.swing.Action;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.*;
+
+//import javax.swing.Action;
 
 public class Activity {
     private String title;
@@ -7,6 +13,11 @@ public class Activity {
     private int clockHour;
     private int clockMinute;
     private Action action;
+    
+    //private static final List<Action> VALUES = Collections.unmodifiableList(Arrays.asList());//values()
+    //private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
 
     public Activity(String title, String description, String location, int clockHour
     , int clockMinute, Action action) {
@@ -16,6 +27,19 @@ public class Activity {
         this.clockHour = clockHour;
         this.clockMinute = clockMinute;
         this.action = action;
+    }
+
+    public void randomAction() { //static Action
+
+        int r = RANDOM.nextInt(9);
+        if(r==0)
+        {
+            System.out.println(Action.ARTS_AND_CRAFTS);
+            
+        }
+        
+        //return VALUES.get(RANDOM.nextInt(SIZE));
+        
     }
 
     public boolean hasAction(Action Action) {
