f80afe0c1b929613f1487f9460ae2674a527fe1c
Marcus Campbell <marcusacampbell03@gmail.com>
2022-10-31 20:40:50 -0400

    adfsa

--- Child.java
+++ Child.java
@@ -20,43 +20,18 @@ public class Child {
     public int monthInt;
     public int dayInt;
     public int yearInt;
-<<<<<<< HEAD
-    private UUID id;
-    /**
-     * Creates a child with new UUID for UI
-     * @param firstName string of the child first name
-     * @param lastName string of the child last name
-     * @param birthday string of the child birthday
-     * @param gender string of the child gender
-     * @param shirtSize string of the child shirt size
-     * @param medName string of the med name
-     * @param medtime string of the med time
-     * @param medDose string of the med dose
-     * @param allergy string of the child allergy
-     */
-    public Child (String firstName, String lastName, String birthday, String gender, String shirtSize, String medName, String medtime, String medDose, String allergy){
-=======
   
 
 
     public Child (String UUID, String firstName, String lastName, String birthday, String gender, String shirtSize, ArrayList<Contact> contacts, ArrayList<Medication> medication){
         this.UUID = UUID;
->>>>>>> 5f9e44bf94530dfabd5618d5329db24c3944dbc4
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthday = birthday;
         this.gender = gender;
         this.shirtSize = shirtSize;
-<<<<<<< HEAD
-        this.medName = medName;
-        this.medtime = medtime;
-        this.medDose = medDose;
-        this.allergy = allergy;
-        this.id = UUID.randomUUID();
-=======
         this.contacts = contacts;
-        this.medication = medication;
->>>>>>> 5f9e44bf94530dfabd5618d5329db24c3944dbc4
+        this.medications = medication;
 
     }
     /**
