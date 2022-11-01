01c7bda55103b66a89945034e951ae1ced8354ef
Eric Avila <arnoldavila2001@gmail.com>
2022-10-31 20:37:09 -0400

    child commit

--- Child.java
+++ Child.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.UUID;
-
+/**
+ * Creates a child
+ */
 public class Child {
     public String firstName;
     public String lastName;
@@ -10,53 +12,111 @@ public class Child {
     public String medtime;
     public String medDose;
     public String allergy;
-    public String strikeCount;
     public String shirtSize;
     public ArrayList<Contact> contacts;
-    public ArrayList<Medication> medication;
+    public ArrayList<Medication> medications;
     public ArrayList<String> allergies;
     public int monthInt;
     public int dayInt;
     public int yearInt;
     private UUID id;
-  
-
-
-    public Child (String firstName, String lastName, String birthday, String gender, String shirtSize, ArrayList<Contact> contacts, ArrayList<Medication> medication){
+    /**
+     * Creates a child with new UUID for UI
+     * @param firstName string of the child first name
+     * @param lastName string of the child last name
+     * @param birthday string of the child birthday
+     * @param gender string of the child gender
+     * @param shirtSize string of the child shirt size
+     * @param medName string of the med name
+     * @param medtime string of the med time
+     * @param medDose string of the med dose
+     * @param allergy string of the child allergy
+     */
+    public Child (String firstName, String lastName, String birthday, String gender, String shirtSize, String medName, String medtime, String medDose, String allergy){
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthday = birthday;
         this.gender = gender;
         this.shirtSize = shirtSize;
-        this.contacts = contacts;
-        this.medication = medication;
+        this.medName = medName;
+        this.medtime = medtime;
+        this.medDose = medDose;
+        this.allergy = allergy;
         this.id = UUID.randomUUID();
 
     }
+    /**
+     * gets the first name of child
+     * @return String firstname
+     */
+    public String getFirstName(){
+        return firstName;
+    }
+    /**
+     * gets lastname of child
+     * @return String lastname
+     */
+    public String getLastName(){
+        return lastName;
+    }
+    /**
+     * gets the birthday of child
+     * @return String birthday
+     */
+    public String getBirthday() {
+        splitBirthday();
+        return birthday;
+    }
+    /**
+     * get the medication name
+     * @return String medname
+     */
+    public String getMedName() {
+        return medName;
+    }
+    /**
+     * gets the medication time
+     * @return String medtime
+     */
+    public String getMedTime() {
+        return medtime;
+    }
+     /**
+     * get the medication dose
+     * @return medDose
+     */
+    public String getMedDose() {
+         return medDose;
+    }
+    /**
+     * get the allergy
+     * @return string allergy
+     */
+    public String getAllergy() {
 
-    public Child(){
-        
+        return allergy;
+    }
+    /**
+     * 
+     * @param allergy
+     */
+    public void addAllergy(String allergy){
+        allergies.add(this.allergy);
     }
 
+    public String getShirtSize() {
+        return shirtSize;
+    }
+ 
     public void addContact (Contact contact){
         contacts.add(contact);
 
     }
 
-    public void strikeCount(){
+    public void addMedication(Medication medication) {
 
-    }
-
-    public void addAllergy(String allergy){
-        allergies.add(this.allergy);
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
+        medications.add(medication);
+        
     }
 
     public void splitBirthday(){
