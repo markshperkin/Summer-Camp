df8f0c31b1d2a41ed8c529899835e6de6d3ca8cd
jiangrick6 <rjiang@email.sc.edu>
2022-11-01 09:41:05 -0400

    updated all versions

--- Director.java
+++ Director.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 public class Director extends User {
    
     private Child camper;
---
+++ Schedule.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+public class Schedule {
+    private ArrayList<Schedule> schedule;
+    private String title;
+    private String descripton;
+    private String location;
+    private int clockhour;
+    private int clockmin;
+    private String timeofday;
+    private String action;
+    private ArrayList<Schedule> Monday;
+    private ArrayList<Schedule>Tuesday;
+    private ArrayList<Schedule> Wednesday;
+    private ArrayList<Schedule>Thursday;
+    private ArrayList<Schedule>Friday;
+    private ArrayList<Schedule>Saturday;
+    private ArrayList<Schedule>Sunday;
+
+    
+}
