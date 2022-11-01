a84c8a6de9c74132eea71bde64556595524ae65f
jiangrick6 <rjiang@email.sc.edu>
2022-10-31 22:17:33 -0400

    updated all versions

--- Director.java
+++ Director.java
@@ -1,14 +1,20 @@
-public class Director {
+public class Director extends User {
+   
     private Child camper;
-    
+
 
     public Director(String fname, String lname, String email, String password, String phoneNum, String street,
-            String town, String zipCode, String state, String country, String birthday) {
-    }
+            String town, String zipCode, String state, String country, String birthday,String gender) {
+    
+                super(fname,lname,email,password,phoneNum,street,town,zipCode,state,country,birthday,gender);
+    
+    
+            }
 
     public String viewCamper(Child camper) {
         return "";
     }
+
 
     public int checkMoney() {
         return 0;
--- DirectorList.java
+++ DirectorList.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
-
-
-
 public class DirectorList {
     private static DirectorList directorList;
     private ArrayList<Director>directors;
@@ -20,6 +17,33 @@ private DirectorList(){
         }
         return directorList;
     }
+public Child addChildren()
+{
+    
+}
+public Parent addParents()
+{
 
+}
+public Counselor addCounselors()
+{
 
+}
+public boolean login(String username, String password) {
+    for(Director d: directors) {
+        if(d.email.equals(username) && d.password.equals(password))
+            return true;
+    }
+    return false;
+}
+
+public void save(){
+    DataWriter.saveDirectors();
+}
+public void save1(){
+    DataWriter.saveParents();
+}
+public void save2(){
+    DataWriter.saveCounselors();
+}
 } 
