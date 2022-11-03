4e6d8899044f690ebcc037ac280cde74e3dca795
Eric Avila <arnoldavila2001@gmail.com>
2022-11-02 22:44:38 -0400

    java doc commit

--- CabinList.java
+++ CabinList.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
 
+/**
+ * Singleton list of cabin
+ */
 public class CabinList {
     private static CabinList cabinList;
     private ArrayList<Cabin> cabins;
-
+    /**
+     * Creates an instance of cabin list
+     */
     private CabinList(){
         cabins = DataReader.getAllCabins();
     }
-
+    /**
+     * gets a cabin list and checks if its null
+     * @return Returns an instance of cabin list
+     */
     public static CabinList getInstance(){
         if (cabinList == null){
             cabinList = new CabinList();
         }
         return cabinList;
     }
-    
+    /**
+     * adds a cabin to array list cabins
+     * @param cabin takes in a cabin
+     * @return returns true or false
+     */
     public boolean addCabin(Cabin cabin) {
         if(cabin == null) 
             return false;
@@ -22,6 +34,11 @@ public class CabinList {
         save();
         return true;
     }
+    /**
+     * gets a cabin through its id
+     * @param id takes in an id of a cabin
+     * @return arraylist cabin
+     */
     public Cabin getCabin(String id) {
         for(Cabin c: cabins) {
             if(c.getID().equals(id))
@@ -30,14 +47,18 @@ public class CabinList {
         return null;
         
     }
-
+    /**
+     * gets all of the cabins
+     */
     public void getAllCabins() {
         for(Cabin c: cabins) {
             System.out.println(c);
             
         }
     }
-
+    /**
+     * saves cabins
+     */
     public void save() {
         DataWriter.saveCabins();
     }
