64fb64cc9869ef4dea103f80b3669a934860e9d7
Marcus Campbell <marcusacampbell03@gmail.com>
2022-11-10 09:41:52 -0500

    testing

--- ParentListTest.java
+++ ParentListTest.java
@@ -8,8 +8,21 @@ import org.junit.jupiter.api.Test;
 
 public class ParentListTest {
     @Test
+
     public void testLoginInvalid(){
         boolean result = ParentsList.getInstance().login("jsmart", "12345");
         assertEquals(false,result);
     }
+    @Test
+    public void testLoginNull() {
+        boolean result = ParentsList.getInstance().login(null, null);
+        assertEquals(false, result);
+    }
+    @Test
+    public void testAddParentToList() {
+        Parent parent = new Parent("Marcus", "Campbell", "mail@email.com", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null");
+    }
+
+
+
 }
