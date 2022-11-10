8906c73b282c5826d64ee44be94471ba4158e820
Marcus Campbell <marcusacampbell03@gmail.com>
2022-11-10 09:30:46 -0500

    Testing

---
+++ ParentListTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ParentListTest {
+    @Test
+    public void testLoginInvalid(){
+        boolean result = ParentsList.getInstance().login("jsmart", "12345");
+        assertEquals(false,result);
+    }
+}
